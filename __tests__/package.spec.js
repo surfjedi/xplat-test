62fae94875de3e2859fd8b781a71f30b397e926d
Lee <leeblazek@gmail.com>
2018-02-02 15:19:57 -0700

    vsreion

--- __tests__/package.spec.js
+++ __tests__/package.spec.js
@@ -1,7 +1,7 @@
 
 describe('pockage.json stuff', () => {
-  test('should have GITTAG.',()=>{
-    expect(process.env.GITTAG).toBe('1.0.1')
+  test('should have VERSION.',()=>{
+    expect(process.env.VERSION).toBe('1.0.1')
   })
   test('should have build.',()=>{
     expect(process.env.BUILD).toBe('222')
