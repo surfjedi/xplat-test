0e3d4e083e8967094af2b930af996472c8447da4
Lee <leeblazek@gmail.com>
2018-02-02 10:15:36 -0700

    add notes

--- __tests__/cross-env-stuff.spec.js
+++ __tests__/cross-env-stuff.spec.js
@@ -1,9 +1,9 @@
 
 describe('cross env vars', () => {
-  test('should have stuff1',()=>{
+  test('should have stuff1. Has escaped qoutes',()=>{
     expect(process.env.STUFF1).toBe('stuff1')
   })
-  test('should have stuff2',()=>{
+  test('should have stuff2. Does not have escaped quotes',()=>{
     expect(process.env.STUFF2).toBe('stuff2')
   })
 });
