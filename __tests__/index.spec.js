faf6471f51b35567e461c431692f88af3e337b8c
Lee <leeblazek@gmail.com>
2018-07-05 16:30:36 -0600

    improve comments

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -1,6 +1,6 @@
 let index = require('../index');
 describe('index', () => {
-  test('should have 1',()=>{
+  test('should have 1 from .env file, if 2 its from .env.',()=>{
     let thing = index.usevars()
     expect(thing).toBe('1')
   })
